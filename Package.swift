29636e0c9bf69d96c7cf4c0c084b8741bd91ecfb
Kentaro Imai <kentaroi@gmail.com>
2023-10-19 22:23:14 +0900

    Increase the minimum deployment target to iOS 13
    I tested the package with the following command, but if failed.
    xcodebuild -scheme EmitterKit test -destination "platform=iOS Simulator,name=iPhone 15,OS=latest"
    
    Testing failed:
            SDK does not contain 'libarclite' at the path
    '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/arc/libarclite_iphonesimulator.a';
    try increasing the minimum deployment target

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "EmitterKit",
     platforms: [
-        .iOS(.v8), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v2)
+        .iOS(.v13), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v2)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
